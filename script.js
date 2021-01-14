afd2a60d089418570b38af6e93138b8e8c02dc32
Ben Brumby <bbrumby00@gmail.com>
2021-01-14 16:01:25 -0500

    fixed JS. Added CSS

--- script.js
+++ script.js
@@ -10,7 +10,7 @@ $(document).ready(function () {
     $("#searchButton").on("click", function(event) {
         event.preventDefault();
 
-    var imageSearch = $("#autocomplete-input").val().trim();
+    var imageSearch = $("#searchInput").val().trim();
     console.log(imageSearch);
     search(imageSearch);
     }
@@ -22,64 +22,43 @@ function search(search) {
     var settings = {
         "async": true,
         "crossDomain": true,
-        "url": "https://www.flickr.com/services/rest/" + search + "?page=1",
+        "url": "https://api.unsplash.com/search/photos?per_page=20&query=" + search + "&client_id=a8ApBN_ZhrUaLUCaIgDZyCOHzxRbQ1wg0kyb7JBgF_Y",
         "method": "GET",
-        "format": "json",
-        "api-key": "d924a2af852b9031787648fee13f8fb8",
-        "safe_search": "3",
-        "per_page": "20",
-        "page": "1",
-        }
+        "content_filter": "low",
+        "per_page": "10",
+        "page": "2",
     }
 
-$.ajax(settings).done(function(response) {
-    console.log(response);
+$.ajax(settings).done(function(data) {
+    $("#searchResults").empty();
+    console.log(data);
 
-    localStorage.setItem("storedSearch", response);
+//     localStorage.setItem("storedSearch", response);
 
 for (i = 0; i < 20; i++) {
 
 //Creating divs for the cards to be put into
-    var zipCodeDiv = $("<div>");
-    zipCodeDiv.addClass("card-container");
-    var zipCodeCardDiv = $("<div>");
-    zipCodeCardDiv.addClass("card");
+    var imageDiv = $("<div>");
+    imageDiv.addClass("card-container");
+    var imageCardDiv = $("<div>");
+    imageCardDiv.addClass("card");
+    var img = $("<img>");
 
 //Creating temporary variables for the application
-    var name = response.result.data[i].restaurant_name;
-    var address = response.result.data[i].address.formatted;
-    var phone = response.result.data[i].restaurant_phone;
-    var price = response.result.data[i].price_range;
-    var foodType = response.result.data[i].cuisines;
+    var imageURL = data.results[i].urls.regular;
+    console.log(imageURL);
+    $(img).attr("src", imageURL);
 
-    // console.log(name);
-    // console.log(address);
-    // console.log(phone);
-    // console.log(price);
-    // console.log(foodType);
+// //Append new outputs to created divs
+    imageDiv.append(img);
+//     zipCodeDiv.append(addressOut);
+//     zipCodeDiv.append(phoneOut);
+//     zipCodeDiv.append(priceOut);
+//     zipCodeDiv.append(foodTypeOut);
 
-    var nameOutput = $("<h5>").text(name);
-    nameOutput.addClass("card-title");
-    var addressOut = $("<p>").text("Address: " + address);
-    addressOut.addClass("cart-text");
-    var phoneOut = $("<p>").text("Phone Number: " + phone);
-    phoneOut.addClass("cart-text");
-    var priceOut = $("<p>").text("Price Level: " + price);
-    priceOut.addClass("cart-text");
-    var foodTypeOut = $("<p>").text("Food Style: " + foodType);
-    foodTypeOut.addClass("cart-text");
+    imageCardDiv.append(imageDiv);
 
-//Append new outputs to created divs
+    $("#searchResults").prepend(imageCardDiv);
 
-    zipCodeDiv.append(nameOutput);
-    zipCodeDiv.append(addressOut);
-    zipCodeDiv.append(phoneOut);
-    zipCodeDiv.append(priceOut);
-    zipCodeDiv.append(foodTypeOut);
-
-    zipCodeCardDiv.append(zipCodeDiv);
-
-    $("#foodSearchResults").prepend(zipCodeCardDiv);
-
-}
-})}})
+// }
+}})}})
